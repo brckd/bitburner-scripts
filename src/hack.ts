ac9d31124204f5655986d567d16f0378f654c2bf
bricked <git@bricked.dev>
2023-10-25 21:03:00 +0200

    add hack script

---
+++ src/hack.ts
@@ -0,0 +1,18 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+  const [target] = ns.args as [string];
+  const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+  const moneyThresh = ns.getServerMaxMoney(target) * 0.5;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    if (ns.getServerSecurityLevel(target) > securityThresh) {
+      await ns.weaken(target);
+    } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+      await ns.grow(target);
+    } else {
+      await ns.hack(target);
+    }
+  }
+}
